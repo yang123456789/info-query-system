84913f9301cc6c8bb9a095c2267b1274cf05794f
yangjiajia <1877084583@qq.com>
2017-07-23 10:53:20 +0800

    添加了注册功能

--- info-query/portal/login.py
+++ info-query/portal/login.py
@@ -1,7 +1,6 @@
 import logging
 import tornado.web
 from views import *
-from mysql import *
 import re
 
 
@@ -19,20 +18,29 @@ class Register(tornado.web.RequestHandler):
         password = self.get_body_argument('password', None)
         phone = self.get_body_argument('phone', None)
         cipher_password = decrypt_password(password)
-        pw = encrypt_password(cipher_password, phone)
-        if not re.match('^[a-zA-Z][a-zA-Z0-9]{3,19}$', username):
-            self.write('username must be startswith latter and endswith alpha length 4-20')
-        elif not re.match('^(?!^\\d+$)(?!^[a-zA-Z]+$)(?!^[_#@]+$).{8,20}', password):
-            self.write('password must be alphanumeric special symbol and length 8-20')
-        elif not re.match('^1(3|4|5|7|8)\d{9}$', phone):
-            self.write('phone invalid')
+        user = 'select {0} from {1}'.format(username, 'customer')
+        ph = 'select {0} from {1}'.format(phone, 'customer')
+        if user:
+            self.write('username exist')
+        if ph:
+            self.write('phone exist')
         else:
-            self.save(username, cipher_password, phone)
-            self.write(200)
+            return self.do_register(username, cipher_password, phone)
+
+    def do_register(self, username, password, phone):
+        if not re.match('[a-zA-Z][a-zA-Z0-9]{3,19}', username):
+            self.write('username startswith latter or chinese length 4-20')
+        elif not re.match('^1(3|4|5|7|8)\d{9}$', phone):
+            self.write('phone Invalid')
+        elif not re.match('^(?!^\\d+$)(?!^[a-zA-Z]+$)(?!^[_#@]+$).{8,20}', password):
+            self.write('password contain latter digest special symbol combine 8-20')
+        else:
+            self.save(username, password, phone)
+            self.write('register success')
 
     def save(self, username, password, phone):
-        statement = '''INSERT INTO customer (username, password, phone) VALUES (
-                    {0}, {1}, {2}
-                    )'''.format(username, password, phone)
-        operation = Operation()
-        operation.create(statement)
+        cipher_password = encrypt_password(password, phone)
+        '''insert into (username, password, phone) VALUES (
+                {0}, {1}, {2}
+                )'''.format(username, cipher_password, phone)
+
